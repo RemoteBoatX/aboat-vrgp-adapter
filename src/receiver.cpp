67530de2bb60c39583f1f61580180b01fb8b9c59
jndzmn <jndzmn@gmail.com>
2022-01-22 16:01:05 +0200

    Add files via upload

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -12,26 +12,32 @@
 #include <thread>
 
 // For writing in the json
+#include <nlohmann/json.hpp>
 #include <string>
-#include <fstream>
+using json = nlohmann::json;         
+
 
 int find_devices() {
-
-
-
+    
+    json devices_array;
+    
     // We open a session and wait for messages to arrive
-    cluon::OD4Session od4{111, [](cluon::data::Envelope &&env){
+    // Didn't find the message of request from the Aboat to the MOC                        
+    cluon::OD4Session od4{111, [&devices_array](cluon::data::Envelope &&env){                
             
-            std::cout << "Data type = " << env.dataType() << std::endl;
+            //TO DO Change to device name if possible
+            std::string device_id = std::to_string(env.dataType());
+            devices_array["ID_" + device_id] = env.dataType();     
             
-            // Didn't find the message of request from the Aboat to the MOC
-            //TO DO -> Write device on json         
+            //Writing on the json file
+            std::ofstream o("devices.json");
+            o << std::setw(4) << devices_array << std::endl;                
         }
     };
 
     // Let's wait for 10s to receive messages.
     using namespace std::chrono_literals;
-    std::this_thread::sleep_for(10s);
+    std::this_thread::sleep_for(15s);
 
     return(0);
 }
